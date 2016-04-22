d369e618b9a9b659212ff3aa96783c92444cf33c
miguel76 <miguel.ceriani@gmail.com>
2016-04-22 14:03:20 -0300

    Setup initial gulp conf

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'), 
+    sass = require('gulp-ruby-sass') 
+    notify = require("gulp-notify") 
+    bower = require('gulp-bower');
+
+var config = {
+  sassPath: './src/css',
+  bowerDir: './bower_components' 
+};
+
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
+});
+
+// for FontAwesome, currently not used
+// gulp.task('icons', function() { 
+//     return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+//         .pipe(gulp.dest('./public/fonts')); 
+// });
+
+gulp.task('octicons', function() { 
+    return gulp.src(config.bowerDir + '/primer/fonts/**.*') 
+        .pipe(gulp.dest('./public/fonts')); 
+});
+
+gulp.task('css', function() { 
+    return gulp.src(config.sassPath + '/style.scss')
+         .pipe(sass({
+             style: 'compressed',
+             loadPath: [
+                 config.sassPath //,
+//                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+//                 config.bowerDir + '/fontawesome/scss',
+             ]
+         }) 
+            .on("error", notify.onError(function (error) {
+                 return "Error: " + error.message;
+             }))) 
+         .pipe(gulp.dest('./public/css')); 
+});
+
+// Rerun the task when a file changes
+ gulp.task('watch', function() {
+     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+});
+
+  gulp.task('default', ['bower', 'icons', 'css']);
